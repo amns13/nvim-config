bf11161e1fd78a7dd588a214eee60ad3841bb2d5
Aman Singh <aman13b8@gmail.com>
2024-07-14 20:48:42 +0530

    Add which-key config

--- lua/amanwa/lazy.lua
+++ lua/amanwa/lazy.lua
@@ -125,6 +125,26 @@ local plugins = {
         end
     },
 
+    -- Whichkey
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    }
+
 }
 
 require("lazy").setup(plugins, opts)
